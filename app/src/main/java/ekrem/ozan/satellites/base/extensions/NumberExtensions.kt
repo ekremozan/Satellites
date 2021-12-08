430541cd1a3da8ac2f1f89f206fb098e9f7bf83a
ekremozanuysal <ekremozanuysal@gmail.com>
2021-12-08 22:08:03 +0300

    Dotted format moved to extension function

--- app/src/main/java/ekrem/ozan/satellites/base/extensions/NumberExtensions.kt
+++ app/src/main/java/ekrem/ozan/satellites/base/extensions/NumberExtensions.kt
@@ -1,3 +1,6 @@
 package ekrem.ozan.satellites.base.extensions
 
+import java.text.DecimalFormat
+
 fun Int?.orZero(): Int = this ?: 0
+fun Int?.toDottedFormat(): String = DecimalFormat("###,###,###").format(this).replace(",",".")
--- app/src/main/java/ekrem/ozan/satellites/ui/detail/DetailViewState.kt
+++ app/src/main/java/ekrem/ozan/satellites/ui/detail/DetailViewState.kt
@@ -1,12 +1,12 @@
 package ekrem.ozan.satellites.ui.detail
 
+import ekrem.ozan.satellites.base.extensions.toDottedFormat
 import ekrem.ozan.satellites.domain.model.SatelliteDetailData
-import java.text.DecimalFormat
 
 data class DetailViewState(val satellite: SatelliteDetailData) {
     fun getName() = satellite.name
     fun getDate() = satellite.firstFlight
     fun getHeightAndMass() = "${satellite.height}/${satellite.mass}"
-    fun getCost() = DecimalFormat("###,###,###").format(satellite.costPerLaunch).replace(",",".")
+    fun getCost() = satellite.costPerLaunch.toDottedFormat()
     fun getPositions() = "${satellite.posX}/${satellite.posY}"
 }
