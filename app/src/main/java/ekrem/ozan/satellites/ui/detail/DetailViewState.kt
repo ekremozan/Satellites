75cf3f302542e381d1b98e15647de440e8b168bb
ekremozanuysal <ekremozanuysal@gmail.com>
2021-12-08 22:04:23 +0300

    Cost amount converted to dotted format

--- app/src/main/java/ekrem/ozan/satellites/ui/detail/DetailViewState.kt
+++ app/src/main/java/ekrem/ozan/satellites/ui/detail/DetailViewState.kt
@@ -1,11 +1,12 @@
 package ekrem.ozan.satellites.ui.detail
 
 import ekrem.ozan.satellites.domain.model.SatelliteDetailData
+import java.text.DecimalFormat
 
 data class DetailViewState(val satellite: SatelliteDetailData) {
     fun getName() = satellite.name
     fun getDate() = satellite.firstFlight
     fun getHeightAndMass() = "${satellite.height}/${satellite.mass}"
-    fun getCost() = satellite.costPerLaunch.toString()
+    fun getCost() = DecimalFormat("###,###,###").format(satellite.costPerLaunch).replace(",",".")
     fun getPositions() = "${satellite.posX}/${satellite.posY}"
 }
